7fe60aece75d1bf0233d5b812fe33d329641a47d
Kirill Yurchenko <kirill.yurchenko07@gmail.com>
2024-06-14 23:03:59 +0300

    Бот+модель

--- chat.py
+++ chat.py
@@ -1,16 +1,19 @@
 import telebot
 import os
+from ultralytics import YOLO
 
 bot = telebot.TeleBot("7250952575:AAElJxVJINJdT3elaUYfFvBNuDTatfxea6A")
+model = YOLO("model/yolov8n.pt")
+model = YOLO("model/train7/weights/best.pt")
 
 
-@bot.message_handler(content_types=['document'])
-def handle_document(message):
-    file_info = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    file_path = os.path.join("userimgs", message.document.file_name)
-    with open(file_path, 'wb') as new_file:
-        new_file.write(downloaded_file)
+# @bot.message_handler(content_types=['document'])
+# def handle_document(message):
+#     file_info = bot.get_file(message.document.file_id)
+#     downloaded_file = bot.download_file(file_info.file_path)
+#     file_path = os.path.join("userimgs", message.document.file_name)
+#     with open(file_path, 'wb') as new_file:
+#         new_file.write(downloaded_file)
 
 
 @bot.message_handler(content_types=['photo'])
@@ -21,13 +24,27 @@ def handle_photo(message):
     file_path = os.path.join("userimgs", f"{file_id}.jpg")
     with open(file_path, 'wb') as new_file:
         new_file.write(downloaded_file)
-    with open(os.path.join("userimgs", "6f46de0337d3ec4b414f1aee92a9cfdb.jpg"), 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
-
+    if RogueAI(file_path):
+        with open("result.png", "rb") as photo:
+            bot.send_photo(message.chat.id, photo)
+        os.remove(os.path.join("userimgs", f"{file_id}.jpg"))
+        os.remove("result.png")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, "Я ОБРАБАТЫВАЮ ФОТОРАФИИ ШВОВ.")
+
+
+def RogueAI(target):
+    results = model(target)
+    for result in results:
+        boxes = result.boxes  # Boxes object for bounding box outputs
+        masks = result.masks  # Masks object for segmentation masks outputs
+        keypoints = result.keypoints  # Keypoints object for pose outputs
+        probs = result.probs  # Probs object for classification outputs
+        obb = result.obb  # Oriented boxes object for OBB outputs # display to screen
+        result.save(filename="result.png")  # save to dis
+    return True
 
 
 bot.polling()
